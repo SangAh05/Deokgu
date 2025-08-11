9152710f601afc49bb7e2832bf11cef10188bc0f
SangAh <tkddk0517@naver.com>
2025-08-11 16:00:00 +0900

    Character Setting

---
+++ Source/Deokgu/Public/Character/DeocGuCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/CharacterBase.h"
+#include "DeocGuCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEOKGU_API ADeocGuCharacter : public ACharacterBase
+{
+	GENERATED_BODY()
+	
+};
